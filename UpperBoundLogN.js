29929d6967ba9e61fd2f6dbc06141ec39341367c
Shivam Singh <shivam_fw12_029@masai.school>
2022-01-22 00:03:04 +0530

    solved

--- UpperBoundLogN.js
+++ UpperBoundLogN.js
@@ -1,42 +1,34 @@
 
-function solve(n, arr, k) {
-    let lo = 0;
+function solve(n, arr, k){
+    let lo = 0 ;
     let hi = n - 1;
     let res = -1;
-
+    
     while (lo <= hi) {
         let mid = Math.floor(lo + (hi - lo) / 2);
 
-        if (arr[mid] === k) 
+        if (arr[mid] <= k)
         {
             res = mid + 1;
-            lo = mid + 1;
+            lo = mid + 1;        
         }
-
-        else if (arr[mid] < k)
-        {
-            lo = mid + 1;
-            res = mid + 1;
-        }
-        else { hi = mid-1}
+        else  { hi = mid - 1 }
+       
     }
-
-    console.log(res)
+    return res;
 }
-
 
 function runProgram(input) {
     // Write code here
     input = input.trim().split("\n");
     let [n, k] = input[0].trim().split(" ").map(Number);
     let arr = input[1].trim().split(" ").map(Number);
-    
-   // console.log(n,arr,k)
-    solve(n,arr,k)
+
+    console.log(solve(n, arr, k));
    
   }
   if (process.env.USERNAME === "Cvam's Singhh") {
-      runProgram(`10 4
+    runProgram(`10 3
 0 2 4 4 5 5 7 7 9 10`);
   } else {
     process.stdin.resume();
--- searchInRotatedArr.js
+++ searchInRotatedArr.js
@@ -1,37 +1,55 @@
 
-function solve(n, arr, k){
-    let lo = 0 ;
-    let hi = n - 1;
-    let res = -1;
-    
+function findmin(n, arr) {
+  let lo = 0;
+  let hi = n - 1; 
+  
+  while (lo <= hi) {
+    let mid = Math.floor(lo + (hi - lo) / 2);
+
+    if (arr[mid] > arr[mid + 1]) { return mid + 1 }
+    else if (arr[mid] < arr[mid - 1]) { return mid }
+    else if (arr[mid] < arr[hi]) { hi = mid - 1 }
+    else ( lo = mid+1)
+  }
+
+  return -1;
+}
+
+
+function solve(arr,lo, hi,k) {     
+
     while (lo <= hi) {
+
         let mid = Math.floor(lo + (hi - lo) / 2);
 
-        if (arr[mid] <= k)
-        {
-            res = mid + 1;
-            lo = mid + 1;        
-        }
-        else if (arr[mid] > k)
-        { hi = mid - 1 }
-       
+      if (arr[mid] === k) { return mid}
+      else if (arr[mid] > k) { hi = mid - 1 }
+      else {lo = mid+1}
+     
     }
-    return res;
-}
+  return -1;
+ }
 
 function runProgram(input) {
     // Write code here
     input = input.trim().split("\n");
     let [n, k] = input[0].trim().split(" ").map(Number);
     let arr = input[1].trim().split(" ").map(Number);
+   
+  let minIndex = findmin(n, arr)
+ // console.log(minIndex);
+  
+  let lo = 0;
+  let hi = n - 1;
 
-   // console.log(n, arr, k)
-    console.log(solve(n, arr, k));
+  if (k > arr[hi])
+  { console.log(solve(arr, lo, minIndex, k)) }
+  else { console.log(solve(arr,minIndex,hi,k))} 
    
   }
   if (process.env.USERNAME === "Cvam's Singhh") {
-    runProgram(`10 3
-0 2 4 4 5 5 7 7 9 10`);
+    runProgram(`5 6
+3 4 5 6 1`);
   } else {
     process.stdin.resume();
     process.stdin.setEncoding("ascii");
